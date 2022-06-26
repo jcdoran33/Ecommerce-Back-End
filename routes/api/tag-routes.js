4b8f33c6d39aa64a0dc9344a90a694ceca493d63
Jack Doran <jcdoran33@gmail.com>
2022-06-26 18:38:50 -0400

    added the same fix into the tag-routes file. Now all routing is working properly

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,7 +38,7 @@ router.post('/', async (req, res) => {
   // create a new tag
   try {
     const newTag = await Tag.create({
-      tag_id: req.body.tag_id
+      tag_name: req.body.tag_name
     });
     res.status(200).json(newTag);
   } catch (err) {
