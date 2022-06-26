b3eb2d4b5dbba233ad11f44f262bc4158a00e3f4
Jack Doran <jcdoran33@gmail.com>
2022-06-26 13:58:06 -0400

    added smeicolon

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ Product.belongsTo(Category, {
 Category.hasMany(Product, {
   foreignKey: "category_id",
   onDelete: "CASCADE"
-})
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
