ba791c7892761903c7e3e5aa077891f3b3765dc1
Jack Doran <jcdoran33@gmail.com>
2022-06-26 14:16:36 -0400

    cleared old comments

--- models/index.js
+++ models/index.js
@@ -17,7 +17,7 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag //what to do here?
+  through: ProductTag
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
